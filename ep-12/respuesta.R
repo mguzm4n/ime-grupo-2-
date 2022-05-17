1c0a3bc75971c8c4c71f215beaab1c38eda5665c
Marcelo <marcelo.guzman.g@usach.cl>
2022-05-17 16:50:26 -0400

    p2

--- ep-12/respuesta.R
+++ ep-12/respuesta.R
@@ -52,6 +52,52 @@ print(prueba$p.value)
 
 # 2.
 
+datos2 <- read.csv2("EP11 Datos.csv")
+
+# Contexto: Se desea realizar una aplicaci?n de citas y se 
+# requiere evaluar los grupos a los que irá enfocada la app.
+# Por ello se desea estudiar el promedio de edad de hombres y mujeres 
+# que están solteros(as) en el rango de edad entre 20 y 40 a?os.
+
+adulto_joven <-  datos2 %>% filter(edad > 20 & edad < 40)
+set.seed(198)
+n <- 350
+adulto_joven <- adulto_joven %>% sample_n(n) %>% select(sexo, ecivil, edad)
+solteros <- adulto_joven %>% filter(ecivil == "Soltero(a)" & sexo == "Hombre")
+solteros_edad <- solteros$edad
+solteras <- adulto_joven %>% filter(ecivil == "Soltero(a)" & sexo == "Mujer")
+solteras_edad <- solteras$edad
+
+edad <- c(solteros_edad, solteras_edad)
+genero <- c(rep("M", length(solteros_edad)), rep("F"  length(solteras_edad)))
+datos_df <- data.frame(edad, genero)
+
+# Comprobar normalidad
+g <- ggqqplot (datos_df, x = "edad",facet.by = "genero" ,
+               palette = c ("blue","red") , color = "genero")
+print(g)
+
+alfa2 <- 0.05
+
+# Ver poda del 20 %
+gamma <- 0.2
+n_a <- length(solteros_edad)
+n_b <- length (solteras_edad)
+poda_a <- n_a*gamma
+poda_b <- n_b*gamma
+a_truncada <- solteros_edad[poda_a:(n_a-poda_a)]
+b_truncada <- solteras_edad[poda_b:(n_b-poda_b)]
+tiempo <- c(a_truncada, b_truncada)
+algoritmo <- c(rep("M", length(a_truncada)), rep("F", length(b_truncada)))
+datos_truncados <- data.frame(edad, genero)
+g <- ggqqplot(datos_truncados, x="edad",facet.by = "genero",
+              palette = c("blue", "red") , color = "genero" )
+print(g) 
+# Aplicar prueba de Yuen
+prueba <- yuen(edad ~ genero, data=datos_df, tr=gamma)
+print(prueba)
+
+
 
 # 3
 
